8c82ac7f3138ae108fe87d2b14558c7ff8e79d33
Your Name <thepolaszek@gmail.com>
2024-03-04 20:36:12 +0100

    working calculator with some bugs

--- script.js
+++ script.js
@@ -1,67 +1,94 @@
-function getSum(a, b) {
-    return a + b;
-}
-
-function getSubstract(a, b) {
-    return a - b;
-}
-
-function getMultiply(a, b) {
-    return a * b;
-}
+let input = '';
+let operatorFlag = 0;
+let resultFlag = 0;
 
 function getDivision(a, b) {
     return (b === 0) ? 0 : a / b;
 }
 
-function getSquare(a) {
-    return a ** 2;
-}
-
-function getRoot(a) {
-    return Math.sqrt(a).toFixed(2);
-}
-
-function getMod(a, b) {
-    return a % b;
-}
-
 function operate(a, operator, b = 0) {
-    let result = 0;
     a = Number(a);
     b = Number(b);
-
+    console.log(`${a} ${operator} ${b}`);
+    let localResult = 0;
     switch (operator) {
         case '+':
-            result = getSum(a, b);
+            localResult = a + b;
             break;
         case '-':
-            result = getSubstract(a, b);
+            localResult = a - b;
             break;
         case '*':
-            result = getMultiply(a, b);
+            localResult = a * b;
             break;
         case '/':
-            result = getDivision(a, b);
+            localResult = getDivision(a, b);
             break;
         case '√':
-            result = getRoot(a);
+            localResult = Math.sqrt(a);
             break;
         case 'x²':
-            result = getSquare(a);
+            localResult = a ** 2;
             break;
-        case '%':
-            result = getMod(a, b);
+        case 'mod':
+            localResult = a % b;
             break;
     }
-    return result;
+    return localResult.toFixed(2);
 }
 
-function main() { //event listeners and global variables
-    let numberA = '3';
-    let numberB = '2';
-    let operator = '%';
-    alert(operate(numberA, operator, numberB));
+function display() {
+    const displayHtml = document.querySelector("#display");
+    displayHtml.textContent = input;
+}
+
+function evaluateKeystroke(n) {
+
+    twoNumberOperationKeys = ['*', '/', '-', '+', 'mod'];
+    numberKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+    singleNumberOperationKeys = ['x²', '√'];
+    if (twoNumberOperationKeys.includes(n)) {
+        if (operatorFlag == 1) {
+            operatorFlag = 0;
+            splitedInput = input.split(' ');
+            input = operate(Number(splitedInput[0]), splitedInput[1], Number(splitedInput[2]));
+            resultFlag++;
+        } else {
+            operatorFlag++;
+            input += ` ${n} `;
+        }
+    } else {
+        if (input === '0' || resultFlag != 0) {
+            input = n;
+            resultFlag = 0;
+        } else
+            if (numberKeys.includes(n))
+                input += n;
+    }
+
+    if (n === 'C') {
+        input = '0';
+    }
+    if (n === '=') {
+        splitedInput = input.split(' ');
+        input = operate(Number(splitedInput[0]), splitedInput[1], Number(splitedInput[2]));
+        resultFlag++;
+    }
+    if (singleNumberOperationKeys.includes(n)) {
+        input = operate(Number(input), n);
+        resultFlag++;
+    }
+
+    display(input)
+}
+
+function main() { //event listeners
+
+    keysArray = document.querySelectorAll("button");
+    for (let i = 0; i < keysArray.length; i++) {
+        keysArray[i].addEventListener('click', () => evaluateKeystroke(keysArray[i].textContent));
+    }
 
 }
-main();
+
+main()
